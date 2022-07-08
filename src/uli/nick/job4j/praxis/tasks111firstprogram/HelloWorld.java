8407870fc1200d88d22def86adf5e8304d85ab79
JavaNickUli <java.nickuli@mail.ru>
2022-07-08 18:30:30 +0300

    HelloWorld. Add tasks.

--- src/uli/nick/job4j/praxis/tasks111firstprogram/HelloWorld.java
+++ src/uli/nick/job4j/praxis/tasks111firstprogram/HelloWorld.java
@@ -1,7 +1,42 @@
 package uli.nick.job4j.praxis.tasks111firstprogram;
 
+import java.util.ArrayList;
+
 /**
- * Каждый оператор в Java должен заканчиваться точкой с запятой. Исправьте ошибку в приведенном коде.
+ * <p>1. Забытая точка с запятой
+ * <p>Каждый оператор в Java должен заканчиваться точкой с запятой. Исправьте ошибку в приведенном коде.
+ *
+ * <p>2. Забытая скобка
+ * <p>Весь код в Java пишется внутри блоков ограниченных фигурными скобками { и }.
+ * Исправьте ошибку в приведенном коде.
+ *
+ * <p>2.1. Пропущенная скобка в блоке классе
+ * <p>Код в Java можно писать только внутри класса. Класс ограничен блоком фигурных скобок.
+ * Ниже приведен код. Поправьте в нем ошибку.
+ *
+ * <p>2.2. Не закрытый класс
+ * <p>Ниже приведен код в котором забыли закрыть блок класса. Поправьте ошибку.
+ *
+ * <p>2.3. Не закрытый метод
+ * <p>В методе main забыли закрывающую скобку. Поправь код приведенный ниже.
+ *
+ * <p>2.4. Скобки только фигурные
+ * <p>Начинающий Java программист решил заменить фигурные скобки на круглые. Так делать нельзя.
+ * Java требует использовать только фигурные скобки для блоков.
+ * Поправьте код приведенный ниже.
+ *
+ * <p>2.5. Python стиль в Java
+ * <p>Программист из Python решил написать код на Java. В Python блоки разделяются отступами,
+ * а в Java фигурными скобками. Поправьте код приведенный ниже.
+ *
+ * <p>2.6. Блоки для методов
+ * <p>В Java класс и метод должны содержать границы блока. Блок ограничивается фигурными скобками.
+ * Поправьте приведенный ниже код.
+ *
+ * <p>3.1. Вывод информации на экран
+ * <p>Чтобы вывести текст на экран в Java используется оператор.
+ * <pre>{@code System.out.println("Hello, Job4j!");}</pre>
+ * Допишите программу приведенную ниже. Программа должна выводить на консоль строку "Hello, Job4j!"
  */
 
 public class HelloWorld {
@@ -10,10 +45,66 @@ public class HelloWorld {
     }
 }
 
-// Каркас:
+// Каркас 1.:
 //public class HelloWorld {
 //    public static void main(String[] args) {
 //        System.out.println("Hello, Job4j!")
+//    }
+//}
+//.
+// Каркас 2.:
+//public class HelloWorld {
+//    public static void main(String[] args)
+//        System.out.println("Hello, Job4j!");
+//    }
+//}
+//.
+// Каркас 2.1.:
+//public class HelloWorld
+//    public static void main(String[] args) {
+//        System.out.println("Hello, Job4j!");
+//    }
+//}
+//.
+// Каркас 2.2.:
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, Job4j!");
+//    }
+//
+//.
+// Каркас 2.3.:
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, Job4j!");
+//
+//}
+//.
+// Каркас 2.4.:
+//public class HelloWorld (
+//    public static void main(String[] args) (
+//        System.out.println("Hello, Job4j!");
+//    )
+//)
+//.
+// Каркас 2.5.:
+//public class HelloWorld
+//    public static void main(String[] args)
+//        System.out.println("Hello, Job4j!");
+//
+//
+//.
+// Каркас 2.6.:
+//public class HelloWorld {
+//    public static void main(String[] args)
+//        System.out.println("Hello, Job4j!");
+//
+//}
+//.
+// Каркас 3.1.:
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//
 //    }
 //}
 //.
