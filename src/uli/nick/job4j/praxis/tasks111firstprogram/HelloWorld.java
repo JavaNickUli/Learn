e7f8d7d93b9ba5446fc87167b7d07f24c068217c
JavaNickUli <java.nickuli@mail.ru>
2022-07-08 17:31:34 +0300

    HelloWorld

---
+++ src/uli/nick/job4j/praxis/tasks111firstprogram/HelloWorld.java
@@ -0,0 +1,19 @@
+package uli.nick.job4j.praxis.tasks111firstprogram;
+
+/**
+ * Каждый оператор в Java должен заканчиваться точкой с запятой. Исправьте ошибку в приведенном коде.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
+
+// Каркас:
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, Job4j!")
+//    }
+//}
+//.
---
+++ test/uli/nick/job4j/praxis/tasks111firstprogram/HelloWorldTest.java
@@ -0,0 +1,23 @@
+package uli.nick.job4j.praxis.tasks111firstprogram;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class HelloWorldTest {
+
+    @Test
+    public void checkOutHelloJob4j() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        HelloWorld.main(null);
+
+        String actual = out.toString();
+        String expected = "Hello, Job4j!" + System.lineSeparator();
+
+        then(actual).isEqualTo(expected);
+    }
+}
