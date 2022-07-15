8cf74f539c1feacec28f90aef1d2e1e3581291e9
JavaNickUli <java.nickuli@mail.ru>
2022-07-15 23:45:54 +0300

    CheckerAndGetter

---
+++ src/test/java/uli/nick/job4j/praxis/tasks131lislarraylist/CheckerAndGetterTest.java
@@ -0,0 +1,36 @@
+package uli.nick.job4j.praxis.tasks131lislarraylist;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class CheckerAndGetterTest {
+
+    @Test
+    void getElementWhenListIsEmptyThenNone() {
+        String actual = CheckerAndGetter.getElement(List.of());
+        String expected = "";
+
+        then(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void getElementWhenListContainsThenFirstString() {
+        String actual = CheckerAndGetter.getElement(List.of("first", "second", "third"));
+        String expected = "first";
+
+        then(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void getElementWhenListIsNullThenFirstString() {
+        try {
+            CheckerAndGetter.getElement(null);
+        } catch (NullPointerException e) {
+            then(e.getMessage()).isEqualTo(
+                    "Cannot invoke \"java.util.List.isEmpty()\" because \"list\" is null");
+        }
+    }
+}
