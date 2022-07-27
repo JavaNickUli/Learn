c032d4c5cc5e88e9d1d15b0b423d758345832ba1
JavaNickUli <java.nickuli@mail.ru>
2022-07-27 17:13:34 +0300

    ArraysTasks

---
+++ src/main/java/uli/nick/mgu/tasks/RemoveFromArrayMoreThenTwo.java
@@ -0,0 +1,33 @@
+package uli.nick.mgu.tasks;
+
+/**
+ * Удалить в массиве все числа, которые повторяются более двух раз.
+ */
+
+public class RemoveFromArrayMoreThenTwo {
+    public static int[] removeMoreThanTwo(int[] array) {
+        int size = array.length;
+        for (int i = 1; i < size; i++) {
+
+            for (int j = 0; j < i; j++) {
+                if (array[i] == array[j]) {
+
+                    int k = i;
+                    for (int l = i + 1; l < size; l++) {
+                        if (array[i] != array[l]) {
+                            k++;
+                            if (k != l) {
+                                array[k] = array[l];
+                            }
+                        }
+                    }
+                    size = k + 1;
+                }
+            }
+        }
+        int[] trimArray = new int[size];
+        System.arraycopy(array, 0, trimArray, 0, size);
+
+        return trimArray;
+    }
+}
---
+++ src/test/java/uli/nick/mgu/tasks/RemoveFromArrayMoreThenTwoTest.java
@@ -0,0 +1,69 @@
+package uli.nick.mgu.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static uli.nick.mgu.tasks.RemoveFromArrayMoreThenTwo.removeMoreThanTwo;
+
+class RemoveFromArrayMoreThenTwoTest {
+
+    @Test
+    void removeMoreThanTwoTest0() {
+        int[] test = {1, 1};
+
+        int[] actual = removeMoreThanTwo(test);
+        int[] expected = {1, 1};
+
+        then(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void removeMoreThanTwoTest1() {
+        int[] test = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+        int[] actual = removeMoreThanTwo(test);
+        int[] expected = {1, 1};
+
+        then(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void removeMoreThanTwoTest2() {
+        int[] test = {-1, 0, 1, 0, -1, 0, -2, 2, -1, 0, 1, 0, -1, 0, -2, 2};
+
+        int[] actual = removeMoreThanTwo(test);
+        int[] expected = {-1, 0, 1, 0, -1, -2, 2, 1, -2, 2};
+
+        then(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void removeMoreThanTwoTest3() {
+        int[] test = {-1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0};
+
+        int[] actual = removeMoreThanTwo(test);
+        int[] expected = {-1, -1, 0, 0, 1, 1};
+
+        then(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void removeMoreThanTwoTest4() {
+        int[] test = {0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7};
+
+        int[] actual = removeMoreThanTwo(test);
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7};
+
+        then(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void removeMoreThanTwoTest5() {
+        int[] test = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+        int[] actual = removeMoreThanTwo(test);
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+        then(actual).isEqualTo(expected);
+    }
+}
