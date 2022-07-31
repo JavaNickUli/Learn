cc93d5862af5502a6b07f571c4866f2a85005fa8
JavaNickUli <java.nickuli@mail.ru>
2022-08-01 01:52:57 +0300

    Quiz

---
+++ src/test/java/uli/nick/stepik/basic/QuizTest.java
@@ -0,0 +1,27 @@
+package uli.nick.stepik.basic;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class QuizTest {
+
+    @Test
+    void mainCheckingOutput() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        try {
+            Quiz.main(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        String actual = output.toString();
+        String expected = "ec5287c45f0e70ec22d52e8bcbeeb640";
+
+        then(actual).isEqualTo(expected);
+    }
+}
