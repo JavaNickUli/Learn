49362ea007457ee9a8e5f345023fc7b7e1a6db05
JavaNickUli <java.nickuli@mail.ru>
2022-08-29 18:31:09 +0300

    PrintTextPerRole

--- src/main/java/uli/nick/stepik/basic/tasks24/PrintTextPerRole.java
+++ src/main/java/uli/nick/stepik/basic/tasks24/PrintTextPerRole.java
@@ -71,6 +71,7 @@ public class PrintTextPerRole {
                 "Артемий Филиппович: Вот не было заботы, так подай!",
                 "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
         System.out.println(printTextPerRole(roles, textLines));
+        System.out.println(per(roles, textLines));
     }
 
     private static String printTextPerRole(String[] roles, String[] textLines) {
@@ -83,12 +84,28 @@ public class PrintTextPerRole {
                 if (textLines[i].startsWith(roles[j] + ":")) {
                     buildText[j].append(i + 1).append(")")
                             .append(textLines[i].substring(roles[j].length() + 1)).append("\n");
+                    break;
                 }
             }
         }
         StringBuilder play = new StringBuilder();
         for (StringBuilder text : buildText) {
             play.append(text).append("\n");
+        }
+        return play.toString();
+    }
+
+    public static String per(String[] roles, String[] textLines) {
+        StringBuilder play = new StringBuilder();
+        for (String role : roles) {
+            play.append(role).append(":\n");
+            for (int j = 0; j < textLines.length; j++) {
+                if (textLines[j].startsWith(role + ":")) {
+                    play.append(j + 1).append(")")
+                            .append(textLines[j].substring(role.length() + 1)).append("\n");
+                }
+            }
+            play.append("\n");
         }
         return play.toString();
     }
