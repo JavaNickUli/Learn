9b8900fb5e51fb31b273f5cd5d0b924a83bcdb16
JavaNickUli <java.nickuli@mail.ru>
2022-10-17 17:41:52 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,23 @@
+package uli.nick.javarush.task.pro.task05.task0510;
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[i + 1];
+            for (int j = 0; j <= i; j++) {
+                result[i][j] = i + j;
+            }
+        }
+
+        for (int[] row : result) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
