54bfae003c1a0adb89f80ab3409e41142f0de013
JavaNickUli <java.nickuli@mail.ru>
2022-10-18 23:57:05 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,31 @@
+package uli.nick.javarush.task.pro.task06.task0605;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+
+        int temp;
+        for (int i = 0; i < array.length / 2; i++) {
+            temp = array[array.length - i - 1];
+            array[array.length - i - 1] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
