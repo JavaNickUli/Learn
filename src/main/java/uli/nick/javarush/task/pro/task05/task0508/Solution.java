84ec114ed601edc1c4f45b25246db5c8e96b2c3b
JavaNickUli <java.nickuli@mail.ru>
2022-10-19 10:58:25 +0300

    Solution

--- src/main/java/uli/nick/javarush/task/pro/task05/task0508/Solution.java
+++ src/main/java/uli/nick/javarush/task/pro/task05/task0508/Solution.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static String[] strings = new String[6];
+    public static String[] strings;
 
     public static void main(String[] args) {
 
+        strings = new String[6];
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < strings.length; i++) {
             strings[i] = scanner.nextLine();
         }
+        scanner.close();
 
         for (int i = 0; i < strings.length; i++) {
             if (strings[i] != null) {
+
                 boolean duplicate = false;
                 for (int j = i + 1; j < strings.length; j++) {
                     if (strings[j] != null && strings[j].equals(strings[i])) {
