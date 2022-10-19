17a3908333123accaae02f4bb85e31bf5665ef46
JavaNickUli <java.nickuli@mail.ru>
2022-10-19 18:49:26 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,13 @@
+package uli.nick.javarush.task.pro.task06.task0608;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long number) {
+
+        return number * number * number;
+    }
+}
