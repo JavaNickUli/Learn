a8c76ab61740a2a0a141b14d651b2c38c5170787
JavaNickUli <java.nickuli@mail.ru>
2022-10-20 17:43:08 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task07/task0715/Entity.java
@@ -0,0 +1,14 @@
+package uli.nick.javarush.task.pro.task07.task0715;
+
+public class Entity {
+
+    public void move() {
+
+        System.out.println("Я передвигаюсь.");
+    }
+
+    public void eat() {
+
+        System.out.println("Я ем.");
+    }
+}
