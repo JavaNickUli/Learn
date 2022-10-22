f08e6b010468e38308bb62805b92572a10e89682
JavaNickUli <java.nickuli@mail.ru>
2022-10-22 16:47:38 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,14 @@
+package uli.nick.javarush.task.pro.task10.task1001;
+
+public class Skyscraper {
+
+    public Skyscraper() {
+
+        System.out.println("Небоскреб построен.");
+    }
+
+    public static void main(String[] args) {
+
+        Skyscraper skyscraper = new Skyscraper();
+    }
+}
