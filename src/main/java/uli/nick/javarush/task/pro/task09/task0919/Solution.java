741c4ca94af996f6692e4053fe039b0e33ee30e5
JavaNickUli <java.nickuli@mail.ru>
2022-10-22 15:41:40 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task09/task0919/Solution.java
@@ -0,0 +1,16 @@
+package uli.nick.javarush.task.pro.task09.task0919;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        String string = "Ходит кот задом наперед";
+        System.out.println(string);
+        System.out.println(reverseString(string));
+    }
+
+    public static String reverseString(String string) {
+
+        return new StringBuilder(string).reverse().toString();
+    }
+}
