284817b0771ed781c0e66b8e30e27a759997ef6e
JavaNickUli <java.nickuli@mail.ru>
2022-10-22 15:13:37 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task09/task0916/Solution.java
@@ -0,0 +1,16 @@
+package uli.nick.javarush.task.pro.task09.task0916;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println(format("Амиго", 5000));
+    }
+
+    public static String format(String name, int salary) {
+
+        String phrase = "Меня зовут %s. Я буду зарабатывать $%d в месяц.";
+
+        return String.format(phrase, name, salary);
+    }
+}
