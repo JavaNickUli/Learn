b0256693c53591af49779d445b5b554bd38df6a0
JavaNickUli <java.nickuli@mail.ru>
2022-10-24 13:37:28 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task11/task1111/Solution.java
@@ -0,0 +1,9 @@
+package uli.nick.javarush.task.pro.task11.task1111;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String string = "Amigo";
+        System.out.println(string);
+    }
+}
