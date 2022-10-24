c76f9c45550c59fc48479d7d659f67a7128a6b61
JavaNickUli <java.nickuli@mail.ru>
2022-10-24 13:35:21 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task11/task1110/Solution.java
@@ -0,0 +1,9 @@
+package uli.nick.javarush.task.pro.task11.task1110;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+    }
+}
