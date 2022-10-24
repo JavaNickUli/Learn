f89626fa073990bed9d9e4db5eaf816102520da2
JavaNickUli <java.nickuli@mail.ru>
2022-10-24 13:11:07 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task11/task1109/Outer.java
@@ -0,0 +1,15 @@
+package uli.nick.javarush.task.pro.task11.task1109;
+
+public class Outer {
+
+    class Inner {
+        Inner() {
+            System.out.println("Создание объекта внутреннего класса");
+        }
+    }
+    static class Nested {
+        Nested() {
+            System.out.println("Создание объекта вложенного класса");
+        }
+    }
+}
