2e97f367c79a908faeb813404284cd9578339fda
JavaNickUli <java.nickuli@mail.ru>
2022-10-26 17:43:23 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task13/task1213/Solution.java
@@ -0,0 +1,23 @@
+package uli.nick.javarush.task.pro.task13.task1213;
+
+import java.util.HashMap;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getProgrammingLanguages());
+    }
+
+    public static HashMap<Integer, String> getProgrammingLanguages() {
+        return new HashMap<>() {{
+            put(0, "Java");
+            put(1, "Kotlin");
+            put(2, "Go");
+            put(3, "Javascript");
+            put(4, "Typescript");
+            put(5, "Python");
+            put(6, "PHP");
+            put(7, "C++");
+        }};
+    }
+}
