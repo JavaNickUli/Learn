da988be02a7c41737431be89b9e3e0fb3a2e5098
JavaNickUli <java.nickuli@mail.ru>
2022-10-27 22:14:15 +0300

    Solution

--- src/main/java/com/javarush/task/task02/task0204/Solution.java
+++ src/main/java/com/javarush/task/task02/task0204/Solution.java
@@ -12,12 +12,12 @@ public class Solution {
     public static class Man {
         public int age;
         public int height;
-        public Woman wife=new Woman();
+        public Woman wife;
     }
 
     public static class Woman {
         public int age;
         public int height;
-        public Man husband=new Man();
+        public Man husband;
     }
 }
