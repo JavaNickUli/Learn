fa7bdf9a5df245fd532b23e86cc651abd15de090
JavaNickUli <java.nickuli@mail.ru>
2022-10-27 13:39:32 +0300

    Solution

---
+++ src/main/java/uli/nick/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,10 @@
+package uli.nick.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for (var numberQuest : JavarushQuest.values()) {
+            System.out.println(numberQuest.ordinal());
+        }
+    }
+}
