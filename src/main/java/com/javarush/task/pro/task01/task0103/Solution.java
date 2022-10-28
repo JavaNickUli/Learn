6702f27a43c8b062897591282275a3d06b7e877b
JavaNickUli <java.nickuli@mail.ru>
2022-10-28 14:18:34 +0300

    Solution

---
+++ src/main/java/com/javarush/task/pro/task01/task0103/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task01.task0103;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String drop = "Сбрасываем";
+        String diego = "Диего";
+        String secret = "сверхсекретный";
+        String cargo = "груз";
+
+        System.out.println(drop + " " + diego);
+        System.out.println(drop + " " + secret + " " + cargo);
+    }
+}
