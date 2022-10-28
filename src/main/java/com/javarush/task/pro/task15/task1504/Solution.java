e8df360aeed6e54b07a26f7f0074c84789347438
JavaNickUli <java.nickuli@mail.ru>
2022-10-28 19:56:29 +0300

    Solution

--- src/main/java/com/javarush/task/pro/task15/task1504/Solution.java
+++ src/main/java/com/javarush/task/pro/task15/task1504/Solution.java
@@ -2,19 +2,16 @@ package com.javarush.task.pro.task15.task1504;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String src = scanner.nextLine();
-        String dest = scanner.nextLine();
-        scanner.close();
-
-        try (var input = Files.newInputStream(Path.of(src));
-             var output = Files.newOutputStream(Path.of(dest))) {
+        try (Scanner scanner = new Scanner(System.in);
+             var input = Files.newInputStream(Paths.get(scanner.nextLine()));
+             var output = Files.newOutputStream(Paths.get(scanner.nextLine()))
+        ) {
             byte[] buffer = new byte[65536];
             while (input.available() > 0) {
                 int real = input.read(buffer);
