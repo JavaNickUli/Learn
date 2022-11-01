19ec147be2a57584aaac7e91daf4e87f5bb3fd8d
JavaNickUli <java.nickuli@mail.ru>
2022-11-01 16:31:50 +0300

    Solution

---
+++ src/main/java/com/javarush/task/pro/task15/task1523/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task15.task1523;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("http://httpbin.org/post");
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.setDoOutput(true);
+        try (OutputStream outputStream = urlConnection.getOutputStream();
+             InputStream inputStream = urlConnection.getInputStream()) {
+            outputStream.write(1);
+            System.out.println(new String(inputStream.readAllBytes()));
+        }
+    }
+}
