0c463383978a25212435912af0d9fbaf0660fc0d
JavaNickUli <java.nickuli@mail.ru>
2022-11-02 16:12:51 +0300

    Solution

---
+++ src/main/java/com/javarush/task/pro/task16/task1619/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task16.task1619;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Solution {
+
+    static LocalDateTime localDateTime = LocalDateTime.now();
+
+    public static void main(String[] args) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyyг. Hч.mмин");
+        System.out.println(dateTimeFormatter.format(localDateTime));
+    }
+}
