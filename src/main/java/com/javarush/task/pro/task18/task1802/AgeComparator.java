3ee13b8056fdc6c243c62fa22823359ade117693
JavaNickUli <java.nickuli@mail.ru>
2022-11-03 16:36:56 +0300

    Solution

--- src/main/java/com/javarush/task/pro/task18/task1802/AgeComparator.java
+++ src/main/java/com/javarush/task/pro/task18/task1802/AgeComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class AgeComparator implements Comparator<Student> {
 
     @Override
-    public int compare(Student o1, Student o2) {
-        return o2.getAge() - o1.getAge();
+    public int compare(Student student1, Student student2) {
+        return student2.getAge() - student1.getAge();
     }
 }
