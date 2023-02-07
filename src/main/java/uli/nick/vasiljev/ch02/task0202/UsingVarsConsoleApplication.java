ad3217a5989b433296bed9d8af95df4dda1c744a
JavaNickUli <java.nickuli@mail.ru>
2023-02-07 07:24:21 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/ch02/task0202/UsingVarsConsoleApplication.java
@@ -0,0 +1,16 @@
+package uli.nick.vasiljev.ch02.task0202;
+
+public class UsingVarsConsoleApplication {
+
+    public static void main(String[] args) {
+        int number = 123;
+        double x = 32.1;
+        char symb = 'A';
+        boolean state = true;
+        System.out.println("Используемые переменные:");
+        System.out.println("Целое число: " + number);
+        System.out.println("Действительное число: " + x);
+        System.out.println("Символ: " + symb);
+        System.out.println("Логическое значение: " + state);
+    }
+}
