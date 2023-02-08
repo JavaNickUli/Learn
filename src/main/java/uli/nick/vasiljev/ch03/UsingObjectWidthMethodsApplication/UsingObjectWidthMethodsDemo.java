a3d8341c966051de50b9bb6778d4cd3cae067476
JavaNickUli <java.nickuli@mail.ru>
2023-02-08 14:19:20 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/ch03/UsingObjectWidthMethodsApplication/UsingObjectWidthMethodsDemo.java
@@ -0,0 +1,32 @@
+package uli.nick.vasiljev.ch03.UsingObjectWidthMethodsApplication;
+
+import javax.swing.*;
+
+class MyClass {
+
+    int number;
+    char symbol;
+
+    void set(int n, char s) {
+        number = n;
+        symbol = s;
+    }
+
+    String getInfo() {
+        String text = "Число: " + number + "\n";
+        text += "Символ: " + symbol;
+        return text;
+    }
+}
+
+public class UsingObjectWidthMethodsDemo {
+
+    public static void main(String[] args) {
+        MyClass objA = new MyClass();
+        MyClass objB = new MyClass();
+        objA.set(100, 'A');
+        objB.set(200, 'B');
+        JOptionPane.showMessageDialog(null, objA.getInfo(), "Первый объект", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, objB.getInfo(), "Второй объект", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
