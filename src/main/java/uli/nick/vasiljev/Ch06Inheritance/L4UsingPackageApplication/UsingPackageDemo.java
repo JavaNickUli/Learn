609d5dc7703454639647b57523de3480f74617c4
JavaNickUli <java.nickuli@mail.ru>
2023-02-18 22:30:56 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/Ch06Inheritance/L4UsingPackageApplication/UsingPackageDemo.java
@@ -0,0 +1,11 @@
+package uli.nick.vasiljev.Ch06Inheritance.L4UsingPackageApplication;
+
+import static javax.swing.JOptionPane.WARNING_MESSAGE;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class UsingPackageDemo {
+
+    public static void main(String[] args) {
+        showMessageDialog(null, "Статический импорт - это удобно!", "Пакет ...L4UsingPackageApplication", WARNING_MESSAGE);
+    }
+}
