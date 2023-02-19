689cb420c7a06ec5547a65aedde7e9e846a0638e
JavaNickUli <java.nickuli@mail.ru>
2023-02-19 11:17:56 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/Ch06Inheritance/L07HidingFieldApplication/HidingFieldDemo.java
@@ -0,0 +1,30 @@
+package uli.nick.vasiljev.Ch06Inheritance.L07HidingFieldApplication;
+
+class Alpha {
+
+    String name;
+}
+
+class Bravo extends Alpha {
+
+    String name;
+
+    void show() {
+        System.out.println("Из класса Alpha: " + super.name);
+        System.out.println("Из класса Bravo: " + name);
+    }
+
+    Bravo(String a, String b) {
+        super();
+        super.name = a;
+        name = b;
+    }
+}
+
+public class HidingFieldDemo {
+
+    public static void main(String[] args) {
+        Bravo obj = new Bravo("alpha", "bravo");
+        obj.show();
+    }
+}
