3f8d04b1996122f4847cf2262de7ec0ab741ac1d
JavaNickUli <java.nickuli@mail.ru>
2023-02-20 13:25:52 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/Ch07AbstractClassesAndInterfaces/L04UsingInterfaceVarsApplication/UsingInterfaceVarsDemo.java
@@ -0,0 +1,45 @@
+package uli.nick.vasiljev.Ch07AbstractClassesAndInterfaces.L04UsingInterfaceVarsApplication;
+
+interface Base {
+
+    void show();
+}
+
+class Alpha implements Base {
+
+    String word;
+
+    Alpha(String txt) {
+        word = txt;
+    }
+
+    public void show() {
+        System.out.println("Объект класса Alpha");
+        System.out.println("Текстовое поле: " + word);
+    }
+}
+
+class Bravo implements Base {
+
+    int number;
+
+    Bravo(int n) {
+        number = n;
+    }
+
+    public void show() {
+        System.out.println("Объект класса Bravo");
+        System.out.println("Целочисленное поле: " + number);
+    }
+}
+
+public class UsingInterfaceVarsDemo {
+
+    public static void main(String[] args) {
+        Base ref;
+        ref = new Alpha("текст");
+        ref.show();
+        ref = new Bravo(123);
+        ref.show();
+    }
+}
