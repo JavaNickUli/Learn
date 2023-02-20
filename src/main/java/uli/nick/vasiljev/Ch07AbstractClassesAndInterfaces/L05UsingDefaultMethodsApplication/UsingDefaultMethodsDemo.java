49811b771d575cdd26320f438ae2e247aeb4c6a2
JavaNickUli <java.nickuli@mail.ru>
2023-02-20 13:38:51 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/Ch07AbstractClassesAndInterfaces/L05UsingDefaultMethodsApplication/UsingDefaultMethodsDemo.java
@@ -0,0 +1,45 @@
+package uli.nick.vasiljev.Ch07AbstractClassesAndInterfaces.L05UsingDefaultMethodsApplication;
+
+interface Base {
+
+    default void show(String txt) {
+        System.out.println("Интерфейс Base: " + txt);
+    }
+
+    void hello();
+}
+
+class Alpha implements Base {
+
+    public void hello() {
+        System.out.println("Объект класса Alpha");
+    }
+
+    public void show(String txt) {
+        System.out.println("Класс Alpha: " + txt);
+    }
+}
+
+class Bravo implements Base {
+
+    public void hello() {
+        System.out.println("Объект класса Bravo");
+    }
+}
+
+public class UsingDefaultMethodsDemo {
+
+    public static void main(String[] args) {
+        Base ref;
+        Alpha objA = new Alpha();
+        objA.hello();
+        objA.show("объектная переменная objA");
+        ref = objA;
+        ref.show("интерфейсная переменная ref");
+        Bravo objB = new Bravo();
+        objB.hello();
+        objB.show("объектная переменная objB");
+        ref = objB;
+        ref.show("интерфейсная переменная ref");
+    }
+}
