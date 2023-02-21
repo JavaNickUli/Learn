1d44cb75b4c548a15b829ee767b9ddf14d96ec81
JavaNickUli <java.nickuli@mail.ru>
2023-02-21 15:42:46 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/Ch07AbstractClassesAndInterfaces/L08MoreExtendingInterfaceApplication/MoreExtendingInterfaceDemo.java
@@ -0,0 +1,37 @@
+package uli.nick.vasiljev.Ch07AbstractClassesAndInterfaces.L08MoreExtendingInterfaceApplication;
+
+interface First {
+
+    default void alpha() {
+        System.out.println("Интерфейс First: метод alpha()");
+    }
+}
+
+interface Second extends First {
+
+    default void alpha() {
+        First.super.alpha();
+        System.out.println("Интерфейс Bravo: метод alpha()");
+    }
+
+    default void bravo() {
+        System.out.println("Интерфейс Bravo: метод bravo()");
+    }
+}
+
+class MyClass implements Second {
+
+    public void bravo() {
+        Second.super.bravo();
+        System.out.println("Класс MyClass: метод bravo()");
+    }
+}
+
+class MoreExtendingInterfaceDemo {
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.alpha();
+        obj.bravo();
+    }
+}
