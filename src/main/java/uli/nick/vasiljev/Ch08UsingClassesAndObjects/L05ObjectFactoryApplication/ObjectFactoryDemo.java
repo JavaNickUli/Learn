e5b38c7e9e6e7268f468bfa00a1b5763487c0d88
JavaNickUli <java.nickuli@mail.ru>
2023-02-22 10:36:57 +0300

    Solution

---
+++ src/main/java/uli/nick/vasiljev/Ch08UsingClassesAndObjects/L05ObjectFactoryApplication/ObjectFactoryDemo.java
@@ -0,0 +1,44 @@
+package uli.nick.vasiljev.Ch08UsingClassesAndObjects.L05ObjectFactoryApplication;
+
+abstract class Base {
+
+    abstract void show();
+}
+
+class Alpha extends Base {
+
+    void show() {
+        System.out.println("Объект класса Alpha");
+    }
+}
+
+class Bravo extends Base {
+
+    void show() {
+        System.out.println("Объект класса Bravo");
+    }
+}
+
+class Charlie extends Base {
+
+    void show() {
+        System.out.println("Объект класса Charlie");
+    }
+}
+
+public class ObjectFactoryDemo {
+
+    static Base createObject(int n) {
+        if (n == 1) return new Alpha();
+        if (n == 2) return new Bravo();
+        return new Charlie();
+    }
+
+    public static void main(String[] args) {
+        Base obj;
+        for (int k = 1; k <= 3; k++) {
+            obj = createObject(k);
+            obj.show();
+        }
+    }
+}
